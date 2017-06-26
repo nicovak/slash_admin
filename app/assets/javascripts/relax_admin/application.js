feed15799f6a242397d53f7eaf85164d84a0d820
KOVACS Nicolas <perso.nkovacs@gmail.com>
2017-06-26 15:40:55 +0200

    add reset date

--- app/assets/javascripts/relax_admin/application.js
+++ app/assets/javascripts/relax_admin/application.js
@@ -20,33 +20,33 @@
 //= require relax_admin/custom
 //= require_tree .
 
-$(document).on('turbolinks:load', init);
+$(document).on("turbolinks:load", init);
 
 function init() {
-  console.log('Code ready to rocks !');
+  console.log("Code ready to rocks !");
 
-  $('.tags').tagsInput({
-    placeholder: 'Ajouter un tag',
-    delimiter: [',',';', ' '],
+  $(".tags").tagsInput({
+    placeholder: "Ajouter un tag",
+    delimiter: [",", ";", " "]
   });
 
   // Scroll to top
   var offset = 250;
   $(window).scroll(function() {
     if ($(this).scrollTop() > offset) {
-      $('.scroll-to-top').fadeIn('slow');
+      $(".scroll-to-top").fadeIn("slow");
     } else {
-      $('.scroll-to-top').fadeOut('slow');
+      $(".scroll-to-top").fadeOut("slow");
     }
   });
 
-  $('.scroll-to-top').click(function(event) {
-    $('html, body').animate({scrollTop: 0}, 300);
+  $(".scroll-to-top").click(function(event) {
+    $("html, body").animate({ scrollTop: 0 }, 300);
   });
 
   // Automatic hide alert
   window.setTimeout(function() {
-    $('.alert').fadeTo(500, 0).slideUp(500, function(){
+    $(".alert").fadeTo(500, 0).slideUp(500, function() {
       $(this).remove();
     });
   }, 1500);
@@ -54,29 +54,30 @@ function init() {
   // clearForm
   $.fn.clearForm = function() {
     return this.each(function() {
-      var type = this.type, tag = this.tagName.toLowerCase();
-      if (tag == 'form')
-        return $(':input',this).clearForm();
-      if (type == 'text' || type == 'password' || tag == 'textarea')
-        this.value = '';
-      else if (type == 'checkbox' || type == 'radio')
-        this.checked = false;
-      else if (tag == 'select')
-        this.selectedIndex = -1;
+      var type = this.type,
+        tag = this.tagName.toLowerCase();
+      if (tag == "form") return $(":input", this).clearForm();
+      if (type == "text" || type == "password" || tag == "textarea")
+        this.value = "";
+      else if (type == "checkbox" || type == "radio") this.checked = false;
+      else if (tag == "select") this.selectedIndex = -1;
     });
   };
 
   // CounterUp plugin
   $.fn.counterUp = function(options) {
-    var settings = $.extend({
-      time: 2000,
-      delay: 10,
-    }, options );
+    var settings = $.extend(
+      {
+        time: 2000,
+        delay: 10
+      },
+      options
+    );
 
     return this.each(function() {
       var initialCounter = 0;
       var counterObject = $(this);
-      var counter = parseInt(counterObject.attr('data-value'));
+      var counter = parseInt(counterObject.attr("data-value"));
       var animation = setInterval(frame, settings.delay);
 
       function frame() {
@@ -84,7 +85,9 @@ function init() {
           counterObject.html(counter);
           clearInterval(animation);
         } else {
-          initialCounter += Math.round((counter / settings.time) * settings.delay);
+          initialCounter += Math.round(
+            counter / settings.time * settings.delay
+          );
           counterObject.html(initialCounter);
         }
       }
@@ -92,107 +95,124 @@ function init() {
   };
 
   // MENU
-  $('.sub-menu').has('.active').parent().addClass('active');
+  $(".sub-menu").has(".active").parent().addClass("active");
 
   // DATA COUNTER
-  $('[data-counter=\'counterup\']').counterUp();
+  $("[data-counter='counterup']").counterUp();
 
   // BULK ACTIONS
-  $('.toggle-all').on('change', function() {
+  $(".toggle-all").on("change", function() {
     var checked = this.checked;
-    $('.table-data-list tbody input[type="checkbox"]').each(function(index, item) {
-      $(item).prop('checked', checked);
+    $('.table-data-list tbody input[type="checkbox"]').each(function(
+      index,
+      item
+    ) {
+      $(item).prop("checked", checked);
     });
   });
 
-  $('.table-data-list input[type="checkbox"]').on('change', function() {
-    var length = $('.table-data-list tbody input[type="checkbox"]:checked').length;
-    $('.batch-current-selected').html(length);
+  $('.table-data-list input[type="checkbox"]').on("change", function() {
+    var length = $('.table-data-list tbody input[type="checkbox"]:checked')
+      .length;
+    $(".batch-current-selected").html(length);
   });
 
   // Delete button protection sweetalert
-  $('.single-delete').on('click', function(e) {
+  $(".single-delete").on("click", function(e) {
     e.preventDefault();
-    var target = $(this).attr('href');
+    var target = $(this).attr("href");
     var current = window.location.href;
 
-    swal({
-      title: 'Êtes-vous sûr ?',
-      text: 'Vous ne serez pas capable de revenir en arrière',
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonClass: 'btn-danger',
-      cancelButtonClass: 'btn-primary',
-      confirmButtonText: 'Oui, supprimer',
-      cancelButtonText: 'Annuler',
-      closeOnConfirm: false,
-    },
-    function() {
-      $.ajax({
-        url: target,
-        method: 'DELETE'
-      }).done(function(){
-        window.location.href = current;
-      });
-    });
-  });
-
-  // Batch action
-  $('.batch-action').on('click', function(e) {
-    e.preventDefault();
-    var target = $(this).data('action');
-    var message = $(this).data('message');
-    var current = window.location.href;
-    var ids = [];
-
-    $('.table-data-list tbody input[type="checkbox"]:checked').each(function(index, checkbox) {
-      ids.push($(this).val());
-    });
-
-    if (ids.length > 0) {
-      swal({
-        title: 'Êtes-vous sûr ?',
-        text: message,
-        type: 'warning',
+    swal(
+      {
+        title: "Êtes-vous sûr ?",
+        text: "Vous ne serez pas capable de revenir en arrière",
+        type: "warning",
         showCancelButton: true,
-        confirmButtonClass: 'btn-danger',
-        cancelButtonClass: 'btn-primary',
-        confirmButtonText: 'Oui, supprimer',
-        cancelButtonText: 'Annuler',
+        confirmButtonClass: "btn-danger",
+        cancelButtonClass: "btn-primary",
+        confirmButtonText: "Oui, supprimer",
+        cancelButtonText: "Annuler",
         closeOnConfirm: false
       },
       function() {
         $.ajax({
           url: target,
-          method: 'post',
-          data: {
-            ids: ids
-          }
-        }).done(function(){
+          method: "DELETE"
+        }).done(function() {
           window.location.href = current;
         });
-      });
+      }
+    );
+  });
+
+  // Batch action
+  $(".batch-action").on("click", function(e) {
+    e.preventDefault();
+    var target = $(this).data("action");
+    var message = $(this).data("message");
+    var current = window.location.href;
+    var ids = [];
+
+    $('.table-data-list tbody input[type="checkbox"]:checked').each(function(
+      index,
+      checkbox
+    ) {
+      ids.push($(this).val());
+    });
+
+    if (ids.length > 0) {
+      swal(
+        {
+          title: "Êtes-vous sûr ?",
+          text: message,
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonClass: "btn-danger",
+          cancelButtonClass: "btn-primary",
+          confirmButtonText: "Oui, supprimer",
+          cancelButtonText: "Annuler",
+          closeOnConfirm: false
+        },
+        function() {
+          $.ajax({
+            url: target,
+            method: "post",
+            data: {
+              ids: ids
+            }
+          }).done(function() {
+            window.location.href = current;
+          });
+        }
+      );
     }
   });
 
-  $('.selectize-single').selectize({});
+  $(".selectize-single").selectize({});
 
-  $('.selectize-multiple').selectize({
+  $(".selectize-multiple").selectize({
     persist: false,
     plugins: {
-      'remove_button': {}
+      remove_button: {}
     }
   });
 
-  $('.bootstrap-material-date').bootstrapMaterialDatePicker({
-    lang: 'fr',
+  $(".bootstrap-material-date").bootstrapMaterialDatePicker({
+    lang: "fr",
     weekStart: 1,
-    cancelText: 'ANNULER',
-    time: false
+    cancelText: "ANNULER",
+    time: false,
+    clearButton: true
   });
 
-  $('.bootstrap-material-datetime').bootstrapMaterialDatePicker({lang: 'fr', weekStart: 1, cancelText: 'ANNULER'});
+  $(".bootstrap-material-datetime").bootstrapMaterialDatePicker({
+    lang: "fr",
+    weekStart: 1,
+    cancelText: "ANNULER",
+    clearButton: true
+  });
 
-  $('.colorpicker').minicolors();
+  $(".colorpicker").minicolors();
   //$(':file').filestyle({buttonBefore: true, buttonText: 'Choisissez un fichier', buttonName: 'btn-primary'});
 }
