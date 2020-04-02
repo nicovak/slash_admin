b3a2353aea3abd952875029016a949d54c326f52
Nicolas Kovacs <perso.nkovacs@gmail.com>
2020-04-02 19:26:24 +0200

    updates

--- lib/slash_admin/engine.rb
+++ lib/slash_admin/engine.rb
@@ -5,9 +5,5 @@ module SlashAdmin
     initializer "slash_admin.assets.precompile" do |app|
       app.config.assets.precompile += %w[slash_admin/*]
     end
-
-    # config.to_prepare do
-    #   Rails.application.config.assets.precompile += %w[slash_admin/*]
-    # end
   end
 end
--- lib/slash_admin/version.rb
+++ lib/slash_admin/version.rb
@@ -1,3 +1,3 @@
 module SlashAdmin
-  VERSION = "1.2.1"
+  VERSION = "1.2.2"
 end
